1b87905db744858d58bfcd3d63ddf8532685cc31
egyteamdev13 <egypt.team.dev13@gmail.com>
2022-11-13 16:12:47 +0200

    fixes

--- pages/preventive-maintenance/add/index.js
+++ pages/preventive-maintenance/add/index.js
@@ -578,7 +578,7 @@ const FormikAdd = () => {
 export default FormikAdd;
 
 // translation ##################################
-export async function getStaticProps({ locale }) {
+export async function getServerSideProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, [
